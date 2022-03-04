dc2fa15b72e9124001d2d86578822829bf3bc209
karanarjunjr <arjunjay2001@gmail.com>
2022-03-04 17:35:50 +0530

    initial commit

--- SASTRAScraper/urls.py
+++ SASTRAScraper/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
 from django.conf.urls import include
 
-# from django.conf import settings
-# from django.views.static import serve
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
 
-
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
