4b1ba5e122759ab47c2e2299a1cd9841d0295340
karanarjunjr <arjunjay2001@gmail.com>
2022-03-04 18:33:23 +0530

    initial commit

--- SASTRAScraper/wsgi.py
+++ SASTRAScraper/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SASTRAScrapper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SASTRAScraper.settings')
 
 application = get_wsgi_application()
