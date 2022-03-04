b6edb023e1faad038aee4b25c38802eb09fc3720
karanarjunjr <arjunjay2001@gmail.com>
2022-03-04 12:21:09 +0530

    initial commit

--- SASTRAScraper/settings.py
+++ SASTRAScraper/settings.py
@@ -71,17 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'SASTRAScraper.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -122,3 +111,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
